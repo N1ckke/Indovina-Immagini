98eec9458eb506b90d218284b7d6ff0c7ad9ef81
Nickke <106338660+N1ckke@users.noreply.github.com>
2023-04-18 09:05:34 +0200

    Update Listener.java

--- IndovinaImmagine/src/Listener.java
+++ IndovinaImmagine/src/Listener.java
@@ -12,50 +12,54 @@ public class Listener implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e){
-        switch (item.type.getName()) {
-            case "Overworld" -> {
-                r.setMin(0);
-                r.setMax(10);
-                r.setExtraction();
-                item.setImageType("Overworld");
-                item.startGame();
-            }
-            case "Nether" -> {
-                r.setMin(10);
-                r.setMax(20);
-                r.setExtraction();
-                item.setImageType("Nether");
-                item.startGame();
+        if(e.getActionCommand().equals(item.btnStart.getText())) {
+            switch (item.type.getName()) {
+                case "Overworld" -> {
+                    r.setMin(0);
+                    r.setMax(10);
+                    r.setExtraction();
+                    item.setImageType("Overworld");
+                    item.startGame();
+                }
+                case "Nether" -> {
+                    r.setMin(10);
+                    r.setMax(20);
+                    r.setExtraction();
+                    item.setImageType("Nether");
+                    item.startGame();
 
-            }
-            case "End" -> {
-                r.setMin(20);
-                r.setMax(30);
-                r.setExtraction();
-                item.setImageType("End");
-                item.startGame();
-            }
-            default -> {
-                item.updateLabel();
-                item.setButton();
-                item.setImage();
-                item.startGame();
+                }
+                case "End" -> {
+                    r.setMin(20);
+                    r.setMax(30);
+                    r.setExtraction();
+                    item.setImageType("End");
+                    item.startGame();
+                }
+                default -> {
+                    item.updateLabel();
+                    item.setButton();
+                    item.setImage();
+                    item.startGame();
+                }
             }
         }
-
-        for(int i = 0; i < 4; i++) {
-            if (item.rb[i].isSelected()) {
-                if (item.rb[i].getText().equals(Item.IMAGE_NAME[item.image])) {
-                    JOptionPane.showMessageDialog(null, "You guessed", "", JOptionPane.INFORMATION_MESSAGE);
-                    item.counterWin++;
-                } else {
-                    JOptionPane.showMessageDialog(null, "You are wrong", "", JOptionPane.ERROR_MESSAGE);
-                    item.counterLose++;
+        if(e.getActionCommand().equals(item.btnConfirm.getText())) {
+            for (int i = 0; i < 4; i++) {
+                if (item.rb[i].isSelected()) {
+                    if (item.rb[i].getText().equals(Item.IMAGE_NAME[item.image])) {
+                        JOptionPane.showMessageDialog(null, "You guessed", "", JOptionPane.INFORMATION_MESSAGE);
+                        item.counterWin++;
+                    } else {
+                        JOptionPane.showMessageDialog(null, "You are wrong", "", JOptionPane.ERROR_MESSAGE);
+                        item.counterLose++;
+                    }
                 }
             }
         }
 
         if((item.counterLose + item.counterWin) == 10) {
+            JOptionPane.showMessageDialog(null, "You guessed" + item.counterWin + "question", "", JOptionPane.INFORMATION_MESSAGE);
             FFile.write(name + ";" + item.win + ";" + item.lose, "src/Players.txt");
             item.setMenu();
         }
